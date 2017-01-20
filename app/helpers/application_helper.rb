c2184cb388c3789b37e72fd479bfad46a1b9acf1
Katie Riley <katieAnRiley@gmail.com>
2017-01-20 11:52:23 -0800

    added redcarpet to use markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,33 @@ module ApplicationHelper
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
   end
+
+  def markdown(text)
+    options ={
+      filter_html:  true,
+      hard_wrap:    true,
+      prettify:     true,
+      space_after_headers: true,
+      fenced_code:  true
+    }
+
+    extensions = {
+      autolink:     true,
+      superscript:  true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    syntax_highlighter(markdown.render(text)).html_safe
+  end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end
 end
